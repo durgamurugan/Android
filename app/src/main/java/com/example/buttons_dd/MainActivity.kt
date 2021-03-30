703effcfdee635fc6c1e2464f12f94ec3f964e14
Durga <durgamargan@gmail.com>
2021-03-30 20:22:24 +0530

    Buttons

---
+++ app/src/main/java/com/example/buttons_dd/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.buttons_dd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fun Add_To_Cart(view: View) {
+            //Add your code here...
+
+        }
+        fun Buy_Now(view: View) {
+            //Add your code here...
+        }
+    }
+}
